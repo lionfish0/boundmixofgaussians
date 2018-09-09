86c758a5da0bdf97d3a98e277b3dc134acce5429
Michael T Smith <lionfishy@gmail.com>
2018-09-09 01:56:48 +0100

    final

--- boundmixofgaussians/__init__.py
+++ boundmixofgaussians/__init__.py
@@ -3,7 +3,7 @@ from scipy import linalg as la
 
 def zeromean_gaussian_1d(x,ls,v):
     """Compute the unnormalised gaussian values at locations specified in X, for a Gaussian
-    centred at the origin with variance ls^2"""
+    centred at the origin with height v, lengthscale ls"""
     twotimesls2 = 2*ls**2
     return v*np.exp(-(x**2)/twotimesls2)
     
@@ -55,6 +55,100 @@ def findbound_lowdim(X,W,ls,v,d,gridspacing,gridstart,gridend,ignorenegatives=Fa
     return maxgridpoint+potential_shortfall
 
 
+
+### New code to make use of negative weights.
+np.set_printoptions(suppress=True,precision=5)
+from boundmixofgaussians import zeromean_gaussian_1d
+
+def compute_sum(w1,w2,x,ls,dist):
+    return zeromean_gaussian_1d(x,ls,w1)+zeromean_gaussian_1d(x-dist,ls,w2)
+def compute_grad(w1,w2,x,ls,dist):
+    return (((x/ls**2)*zeromean_gaussian_1d(x,ls,w1))+(((x-dist)/ls**2)*zeromean_gaussian_1d(x-dist,ls,w2)))
+
+
+def findpeak(w1,w2,dist,ls):
+    """
+    Find the peak location and value of the sum of a positive and a negative
+    weighted pair of Gaussians, with the positive Gaussian centred at x=0, and
+    the negatively weighted Gaussian centred at x=+dist.
+       w1 is positive weighted, w2 is negative weighted. 
+       ls is the lengthscale.
+       
+    Returns the location of the peak of the sum, and its value.
+    
+ resulting    |
+ location of  |.............
+ sum's peak ..|................w1=0.5
+     .........|.......................        dist=2
+    ----------|------+------------------------+------------------------------
+           -0.42  ....................................................
+                     ..........................................
+      ls=1.0             ...................................
+                             ...........................w2=-1.5
+                                 ..................
+      
+    For example:
+        x, p = findpeak(0.5,-1.5,2.0,1.0)
+    will have x=-0.42, p=0.377
+    """
+    #print(w1,w2,dist,ls)
+    assert w1>0
+    assert w2<0
+    x = 0
+    lr = 0.7
+    #for it in range(20):
+    oldx = np.inf
+    while np.abs(oldx-x)>1e-3: #TODO Pick something sensible for this
+        oldx = x
+        x-=lr*compute_grad(w1,w2,x,ls,dist)
+        lr*=0.97 #TODO and this
+        
+    return x, compute_sum(w1,w2,x,ls,dist)
+
+def mergenegatives(X,W,ls):
+    
+    i = -1
+    #for i,(x,w) in enumerate(zip(X,W)): #find all negatives
+    #print("|",end="")
+    while(i<len(X)-1):
+
+        i+=1
+        x = X[i,:]
+        w = W[i]   
+        
+        if w>=0:
+            continue
+        #print(",",end="")            
+        sqrdists = np.sum((x-X)**2,1)
+        sqrdists[W<=0] = np.infty #we want to combine with a positive weight
+        nearestpositive = np.argmin(sqrdists)
+        dist = np.sqrt(sqrdists[nearestpositive])
+        if dist==np.infty: #all points are now negative!
+            #print(">>>",end="")
+            break
+        offset,newpeak = findpeak(W[nearestpositive],w,dist,ls)
+        #print(offset,newpeak)
+        vector = (x - X[nearestpositive,:])
+        #offset value is positive in direction from +ve weight to -ve weight
+        newx = (X[nearestpositive,:] + offset * vector/dist)
+        #delete nearestpositive and i from X and W
+        #add newx and newpeak to X and W
+        #do so by replacing one, deleting other
+        #
+        #so in effect, move +ve one:
+        X[nearestpositive,:] = newx
+        W[nearestpositive] = newpeak
+        #delete negative one:
+        #X = np.delete(X,i,0)
+        #W = np.delete(W,i,0)
+        #for speed we'll just set W to zero, and delete later...
+        W[i] = 0
+    X = np.delete(X,np.where(W==0),0)
+    W = np.delete(W,np.where(W==0),0)
+    return (X,W)
+
+
+
 def findbound(X,W,ls,d,gridres,gridstart,gridend,fulldim=False,forceignorenegatives=False,dimthreshold=3):
     """
     X = input locations
@@ -74,8 +168,26 @@ def findbound(X,W,ls,d,gridres,gridstart,gridend,fulldim=False,forceignorenegati
     v = 1 #this is set to one, as we don't want to scale the Gaussians again!
     
     assert len(gridstart)==d, "Gridstart & gridend should have same number of items as the number of dimensions (%d)" % d
+    
+    #if two points are on top of each other, we can combine them.
+    #particular useful if either of them is negative, as at high
+    #dimensionality we drop the negative points.
+    #!For our GPAdversarial bounds method this is already done
+    #so it could be left out, but points colocated causes problems
+    #with zero-distances in mergenegatives, so I've left it in.
+    #
+    newX = []
+    newW = []
+    for x in np.unique(X,axis=0):
+        newX.append(x)
+        newW.append(np.sum(W[(np.where((X==x).all(axis=1))[0])]))
+    X = np.array(newX)
+    W = np.array(newW)
+    X,W = mergenegatives(X,W,ls)
+
+    
     if X.shape[1]>dimthreshold and not fulldim:
-        #print("Compacting to 3d manifold...")
+        #print("Compacting to %d manifold..." % dimthreshold)
         lowd = dimthreshold
         lowdX,evals,evecs,means = PCA(X.copy(),lowd)
         ignorenegatives = True
